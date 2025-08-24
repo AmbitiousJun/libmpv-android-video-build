7580fc24452f34f1ebf76b4b27135d615bacbd1b
ambitious <1535820771@qq.com>
2025-08-24 18:16:52 +0800

    first commit

--- buildscripts/bundle_full.sh
+++ buildscripts/bundle_full.sh
@@ -30,32 +30,55 @@ sudo chmod +x gradlew
 
 unzip -o app/build/outputs/apk/release/app-release.apk -d app/build/outputs/apk/release
 
-mkdir -p temp_dir/lib/arm64-v8a
-mkdir -p temp_dir/lib/armeabi-v7a
-mkdir -p temp_dir/lib/x86
-mkdir -p temp_dir/lib/x86_64
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/arm64-v8a"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/armeabi-v7a"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86_64"
 
-cp app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so      temp_dir/lib/arm64-v8a/
-cp app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so    temp_dir/lib/armeabi-v7a/
-cp app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so            temp_dir/lib/x86/
-cp app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so         temp_dir/lib/x86_64/
+cd ../..
 
-cp ../../prefix/arm64-v8a/usr/local/lib/*.so temp_dir/lib/arm64-v8a
-cp ../../prefix/armeabi-v7a/usr/local/lib/*.so temp_dir/lib/armeabi-v7a
-cp ../../prefix/x86/usr/local/lib/*.so temp_dir/lib/x86
-cp ../../prefix/x86_64/usr/local/lib/*.so temp_dir/lib/x86_64
+# --------------------------------------------------
 
-cd temp_dir
+cd deps/media_kit/media_kit_native_event_loop
 
-zip -r full-arm64-v8a.jar                lib/arm64-v8a/*.so
-zip -r full-armeabi-v7a.jar              lib/armeabi-v7a/*.so
-zip -r full-x86.jar                      lib/x86/*.so
-zip -r full-x86_64.jar                   lib/x86_64/*.so
+flutter create --org com.alexmercerind --template plugin_ffi --platforms=android .
+
+if ! grep -q android "pubspec.yaml"; then
+  printf "      android:\n        ffiPlugin: true\n" >> pubspec.yaml
+fi
+
+flutter pub get
+
+cp -a ../../mpv/libmpv/. src/include/
+
+cd example
+
+flutter clean
+flutter build apk --release
+
+unzip -o build/app/outputs/apk/release/app-release.apk -d build/app/outputs/apk/release
+
+cd build/app/outputs/apk/release/
+
+# --------------------------------------------------
+
+rm -r lib/*/libapp.so
+rm -r lib/*/libflutter.so
+
+zip -r "full-arm64-v8a.jar"                lib/arm64-v8a
+zip -r "full-armeabi-v7a.jar"              lib/armeabi-v7a
+zip -r "full-x86.jar"                      lib/x86
+zip -r "full-x86_64.jar"                   lib/x86_64
+
+mkdir -p ../../../../../../../../../../output
+
+cp *.jar ../../../../../../../../../../output
 
 md5sum *.jar
 
-mkdir -p /home/runner/work/libmpv-android-video-build/libmpv-android-video-build/output
-cp *.jar /home/runner/work/libmpv-android-video-build/libmpv-android-video-build/output/
+cd ../../../../../../../../..
 
-echo "Output dir: $(realpath /home/runner/work/libmpv-android-video-build/libmpv-android-video-build/output)"
-echo "Script dir: $(cd "$(dirname "$0")"; pwd)"
+# --------------------------------------------------
+
+zip -r debug-symbols-full.zip prefix/*/lib
+cp debug-symbols-full.zip ../output
